d121556657dacd358298ceff3e05a0841581f550
Jaroslav Malec <dzardacz@gmail.com>
2021-11-05 19:33:29 +0100

    Adapt to mmaster, add Makefile

--- include/chconf.h
+++ include/chconf.h
@@ -31,7 +31,7 @@
 // clang-format off
 
 #define _CHIBIOS_RT_CONF_
-#define _CHIBIOS_RT_CONF_VER_6_1_
+#define _CHIBIOS_RT_CONF_VER_7_0_
 
 /*===========================================================================*/
 /**
--- include/halconf.h
+++ include/halconf.h
@@ -29,8 +29,8 @@
 #define HALCONF_H
 
 #define _CHIBIOS_HAL_CONF_
-// #define _CHIBIOS_HAL_CONF_VER_8_0_
-#define _CHIBIOS_HAL_CONF_VER_7_1_
+#define _CHIBIOS_HAL_CONF_VER_8_0_
+// #define _CHIBIOS_HAL_CONF_VER_7_1_
 
 #include "mcuconf.h"
 
